c894407bb02965915a9f82dfad4b02d9083b738c
GNU_CS_UngSik <87903147+blpeng2@users.noreply.github.com>
2022-07-20 20:08:58 +0000

    get_children_blocks_text

--- notion/block.py
+++ notion/block.py
@@ -34,3 +34,18 @@ class Block(Client):
         for result in response.json()["results"]:
             arr.append(result)
         return arr
+
+    def get_children_blocks_text(self) -> list:
+        url = f"https://api.notion.com/v1/blocks/{self.block_id}/children?page_size=100"
+
+        headers = {
+            "Accept": "application/json",
+            "Notion-Version": "2022-06-28",
+            "Authorization": f"Bearer {self.api_key}"
+        }
+        arr = []
+        response = requests.get(url, headers=headers)
+        for result in response.json()["results"]:
+            arr.append(result["paragraph"]["rich_text"][0]["text"]
+                       ["content"] if result["paragraph"]["rich_text"] else None)
+        return arr

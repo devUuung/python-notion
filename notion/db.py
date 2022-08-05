14e2c09b7bfae9342adfb4d3078374c40c257ef6
GNU_CS_UngSik <87903147+blpeng2@users.noreply.github.com>
2022-08-05 19:55:29 +0000

    다른 속성으로 삭제,수정기능

--- notion/db.py
+++ notion/db.py
@@ -1,8 +1,68 @@
 import requests
 
 
+class Page:
+    def __init__(self, json, attrs: dict) -> None:
+        self.id = json["id"]
+        self.attr = {}
+        for key, property in json["properties"].items():
+            if property["type"] == "rich_text":
+                self.attr[key] = {
+                    "value": property["rich_text"][0]["plain_text"] if property["rich_text"] else None,
+                    "type": attrs[key].type,
+                    "primary": attrs[key].primary,
+                    "notNone": attrs[key].notNone,
+                    "title": False,
+                    "rich_text": True
+                }
+            elif property["type"] == "title":
+                self.attr[key] = {
+                    "value": property["title"][0]["plain_text"] if property["title"] else None,
+                    "type": attrs[key].type,
+                    "primary": attrs[key].primary,
+                    "notNone": attrs[key].notNone,
+                    "title": True,
+                    "rich_text": False
+                }
+
+
+class Attribute:
+    """
+        Attribute 클래스로 만들어진 인스턴스의 네이밍은 key의 이름이어야합니다.
+    """
+
+    def __init__(self, type, **kwargs) -> None:
+        """
+        type 종류
+        1. str, string
+        2. int, integer
+        3. float
+        """
+        self.type = type
+        try:
+            if kwargs["primary"] == True:
+                self.primary = True
+                self.notNone = True
+            else:
+                self.primary = False
+                self.notNone = False
+        except KeyError:
+            self.primary = False
+            self.notNone = False
+        try:
+            if kwargs["notNone"] and kwargs["notNone"] == True:
+                self.notNone = True
+        except KeyError:
+            ...
+
+
 class Database:
-    def __init__(self, url, api) -> None:
+    def __init__(self, url, api, attrs: dict) -> None:
+        """
+        url: 데이터베이스가 존재하는 http 주소입니다.
+        api: notionAPI key 입니다.
+        attrs: Attribute 클래스 인스턴스들의 딕셔너리 묶음입니다.
+        """
         headers = {
             "Accept": "application/json",
             "Notion-Version": "2022-06-28",
@@ -11,15 +71,16 @@ class Database:
         res = requests.get(
             "https://api.notion.com/v1/databases/" +
             url.split("/")[-1].split("?")[0],
-            headers=headers).json()
+            headers=headers
+        ).json()
 
         self.id = res["id"]
         self.title = res["title"]
         self.properties = res["properties"]
         self.api = api
+        self.attrs = attrs
 
-    # 정상작동
-
+    # insert할때 타입이 일치하는지 확인하는 기능 추가해야함.
     def insert(self, attributes, contents):
         url = "https://api.notion.com/v1/pages"
         properties = {}
@@ -48,9 +109,12 @@ class Database:
         res = requests.post(url, json=payload, headers=headers)
         return res.json()
 
-    # 정상작동
-
-    def read(self):
+    def read(self) -> tuple[Page]:
+        """
+        Page 클래스 튜플을 반환합니다.
+        1. id
+        2. attr
+        """
         url = f"https://api.notion.com/v1/databases/{self.id}/query"
         payload = {"page_size": 100}
         headers = {
@@ -60,19 +124,16 @@ class Database:
             "Authorization": f"{self.api}"
         }
         res = requests.post(url, json=payload, headers=headers).json()
-        return res
+        arr = []
+        for result in res["results"]:
+            arr.append(Page(result, self.attrs))
+        return tuple(arr)
 
-    # 정상작동
-
-    def update(self, primary, before_content, after_content):
-        response = self.read()
-        for result in response["results"]:
-            if result["properties"][f"{primary}"]["title"][0]["plain_text"] == before_content:
-                page_id = result["url"].split("/")[-1].split("-")[-1]
-                url = f"https://api.notion.com/v1/pages/{page_id}"
+    def update(self, key, before_content, after_content) -> bool:
+        for result in self.read():
+            if result.attr[key]["value"] == before_content:
+                url = f"https://api.notion.com/v1/pages/{result.id}"
                 properties = {}
-                properties[primary] = {
-                    "title": [{"text": {"content": after_content}}]}
                 payload = {
                     "parent": {
                         "type": "database_id",
@@ -86,20 +147,22 @@ class Database:
                     "Content-Type": "application/json",
                     "Authorization": f"{self.api}"
                 }
-                res = requests.patch(
-                    url, json=payload, headers=headers).json()
+                if result.attr[key]["title"]:
+                    properties[key] = {
+                        "title": [{"text": {"content": after_content}}]}
+                elif result.attr[key]["rich_text"]:
+                    properties[key] = {
+                        "rich_text": [{"text": {"content": after_content}}]}
+                res = requests.patch(url, json=payload, headers=headers).json()
         return True if res else False
 
-    # 정상작동
-    # TODO primary뿐만 아니라 다른 속성으로도 삭제할 수 있게 만들기
-
-    def delete(self, primary, content):
-        response = self.read()
-        # primary를 통해 삭제할 요소를 찾음
-        for result in response["results"]:
-            if result["properties"][f"{primary}"]["title"][0]["plain_text"] == content:
-                page_id = result["url"].split("/")[-1].split("-")[-1]
-                url = f"https://api.notion.com/v1/pages/{page_id}"
+    def delete(self, key, content) -> bool:
+        """
+        key의 값이 content인 요소를 삭제합니다.
+        """
+        for result in self.read():
+            if result.attr[key]["value"] == content:
+                url = f"https://api.notion.com/v1/pages/{result.id}"
                 payload = {"archived": True}
                 headers = {
                     "Accept": "application/json",

111d35d689990396ceceb88750c4094a923214a2
GNU_CS_UngSik <blpeng@naver.com>
2022-08-25 01:07:00 +0900

    refactor: property
    property 클래스 분류
    
    2022-08-25

--- notion/response.py
+++ notion/response.py
@@ -1,23 +1,5 @@
 import requests
-
-class Property:
-  def __init__(self, key, value) -> None:
-    self.key = key
-    self.type = value["type"]
-    self.id = id
-    match value["type"]:
-      case "title":
-        self.value = value["title"][0]["plain_text"]
-      case "rich_text":
-        self.value = value["rich_text"][0]["plain_text"]
-      case _:
-        raise Exception("Property 에러")
-
-  def __call__(self) -> dict:
-    return {
-      "content": self.value,
-      "type": self.type
-    }
+from notion.property import Property
 
 class Response:
   def __init__(self, url, json, headers) -> None:
